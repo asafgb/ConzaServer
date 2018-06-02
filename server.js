7781821bf7ab9f901cf0be3713c76c37435ae2dc
asafgb <asafshazar@gmail.com>
2018-06-02 20:50:32 +0300

    mongo

--- server.js
+++ server.js
@@ -29,10 +29,88 @@ app.get('/api/customers',(req, res) =>{
 })
 
 
+/*
+Create Private Key and Public
+
+*/
 var RSAKey = require('react-native-rsa');
 const bits = 1024;
 const exponent = '10001'; // must be a string
 var rsa = new RSAKey();
 var r = rsa.generate(bits, exponent);
-var publicKey = rsa.RSAGetPublicString(); // return json encoded string
-var privateKey = rsa.RSAGetPrivateString(); // return json encoded string
+var publicKey = rsa.getPublicString();//rsa.RSAGetPublicString(); // return json encoded string
+var privateKey = rsa.getPrivateString();//rsa.RSAGetPrivateString(); // return json encoded string
+
+
+
+
+/*
+Connect to Mongi
+
+
+*/
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+//const url = 'mongodb://AsafUser:wc4LoXqMDqoKYRXS@cluster0-shard-00-00-8exny.mongodb.net:27017,cluster0-shard-00-01-8exny.mongodb.net:27017,cluster0-shard-00-02-8exny.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+//const url = 'mongodb+srv://Asafgb:Asafgb@cluster0-8exny.mongodb.net/test?retryWrites=true'
+const url ='mongodb+srv://Tester:Tester@cluster0-8exny.mongodb.net/test'
+// Database Name
+const dbName = 'asafgb';
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+      {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+  }
+
+
+  const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(docs)
+      callback(docs);
+    });
+  }
+
+// Use connect method to connect to the server
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    insertDocuments(db, function() {
+        findDocuments(db, function() {
+          client.close();
+        });
+      });
+  });
+
+
+
+  
+
+// var rsa = new RSAKey();
+// rsa.setPublicString(publicKey);
+// var originText = 'sample String Value';
+// var encrypted = rsa.encrypt(originText);
+
+
+// rsa.setPrivateString(privateKey);
+// var decrypted = rsa.decrypt(encrypted); // decrypted == originText
